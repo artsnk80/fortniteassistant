ffe392e5aeb8c323c85681ffb21c5b60c591dad3
artsnk80 <alertrutaxi@gmail.com>
2021-01-26 22:49:59 +0500

    filters

--- fortniteassistant/ViewController.swift
+++ fortniteassistant/ViewController.swift
@@ -6,6 +6,7 @@
 // github token ab807b088035741918e479cc3136794c18af9736
 
 import UIKit
+
 class tableViewCell: UITableViewCell {
     @IBOutlet weak var labelCell: UILabel!
     @IBOutlet weak var imageType: UIImageView!
@@ -13,12 +14,28 @@ class tableViewCell: UITableViewCell {
     @IBOutlet weak var imageGroup: UIImageView!
     @IBOutlet weak var imagePower: UIImageView!
 }
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
     @IBOutlet weak var label4: UILabel!
     @IBOutlet weak var labelSum: UILabel!
+    @IBOutlet weak var viewFilter: UIView!
+    @IBOutlet weak var switchZone0: UISwitch!
+    @IBOutlet weak var switchZone1: UISwitch!
+    @IBOutlet weak var switchZone2: UISwitch!
+    @IBOutlet weak var switchZone3: UISwitch!
+    @IBOutlet weak var switchCategory0: UISwitch!
+    @IBOutlet weak var switchCategory1: UISwitch!
+    @IBOutlet weak var switchCategory2: UISwitch!
+    @IBOutlet weak var switchCategory3: UISwitch!
+    @IBOutlet weak var switchSingle: UISwitch!
+    @IBOutlet weak var switchGroup: UISwitch!
+    @IBOutlet weak var switchVBucks: UISwitch!
+    @IBOutlet weak var switchPerkup: UISwitch!
+    @IBOutlet weak var switchEpic: UISwitch!
+    @IBOutlet weak var switchLegendary: UISwitch!
     @IBOutlet weak var navigationTitle: UINavigationItem!
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var titleLabel: UILabel!
@@ -47,6 +64,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var quantity: Int = 0
     }
     struct MissionFlat {
+        var locationName: String = ""
         var categoryId: Int = 0
 //        var location: Int = 0
         var typeId: Int = 0
@@ -55,16 +73,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var powerId: Int = 0
         var item: Item = Item()
      }
+    var btn = UIButton(type: .custom)
 
     var expire: CLong?
-    var zones = [
-        [MissionFlat](),  // Камнелесье
-        [MissionFlat](),  // Планкертон
-        [MissionFlat](),  // Вещая долина
-        [MissionFlat]()   // Линч Пикс
+    var zones = [[MissionFlat]()
+ //       [MissionFlat](),  // Камнелесье
+ //       [MissionFlat](),  // Планкертон
+ //       [MissionFlat](),  // Вещая долина
+ //       [MissionFlat]()   // Линч Пикс
     ]
     var vbucksSum = [Int](arrayLiteral: 0,0,0,0)
-    
+    var filterZones = [Bool](arrayLiteral: true,true,true,true)
+    var filterCategories = [Bool](arrayLiteral: true,true,true,true)
+    let zonesNames = [String](arrayLiteral: "Камнелесье","Планкертон","Вещая Долина","Линч Пикс")
     func UIColorFromRGB(rgbValue: UInt) -> UIColor {
         return UIColor(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
@@ -77,6 +98,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                           0x50276b,// "epicItem"
                           0x388E3C,// "uncommonItem"
                           0xFB8C00]// "legendaryItem"
+    let button = UIButton(frame: CGRect(x: 150, y: 550, width: 75, height: 75))
 
  // powerId начинается с 1
     var powerVal: [Int] = [1,3,5,9,15,19,23,28,34,40,46,52,58,64,70,76,82,88,94,100,108,116,124,132,140,160]
@@ -111,6 +133,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         titleLabel.text = NSLocalizedString("element_vbucks_header", comment: "")
         _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimer), userInfo: nil, repeats: true)
         setTitle(newTitle: "dfff")
+            btn.frame = CGRect(x: 285, y: 485, width: 80, height: 80)
+            btn.setTitle("Filters", for: .normal)
+            btn.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            btn.clipsToBounds = true
+            btn.layer.cornerRadius = 40
+            btn.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            btn.layer.borderWidth = 3.0
+            btn.addTarget(self,action: #selector(onFilter), for: .touchUpInside)
+            view.addSubview(btn)
+        viewFilter.isHidden = true
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        downSwipe.direction = .down
+        upSwipe.direction = .up
+
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(downSwipe)
+        view.addGestureRecognizer(upSwipe)
+
+    }
+    
+    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer) {
+        print("swipe")
+        if  viewFilter.isHidden == false {
+            viewFilter.isHidden = true
+            btn.isHidden = false
+        }
+    }
+    @objc func onFilter() {
+        print("onFilter")
+        viewFilter.isHidden = false
+        btn.isHidden = true
     }
     
     @objc func onTimer() {
@@ -127,10 +187,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
 print("section \(section)")
 let label = UILabel()
-        if section == 0 {label.text = "Камнелесье"}
-        if section == 1 {label.text = "Планкертон"}
-        if section == 2 {label.text = "Вещая Долина"}
-        if section == 3 {label.text = "Линч Пикс"}
+        if  self.zones[section].indices.contains(0), // isIndexValid
+            let locName = self.zones[section][0].locationName as String? {
+            label.text = locName
+        } else
+        {
+            label.text = "<>"
+        }
+ //       if section == 0 {label.text = "Камнелесье"}
+ //       if section == 1 {label.text = "Планкертон"}
+ //       if section == 2 {label.text = "Вещая Долина"}
+ //       if section == 3 {label.text = "Линч Пикс"}
 
         label.backgroundColor = UIColor.lightGray
         return label
@@ -146,12 +213,12 @@ let label = UILabel()
         return i
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("add to tableView at secytion=\(indexPath.section),index=\(indexPath.row)")
+        print("add to tableView at section=\(indexPath.section),index=\(indexPath.row)")
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! tableViewCell
         let m = self.zones[indexPath.section][indexPath.row]
    //     cell.textLabel?.numberOfLines = 2
+        print(m)
         cell.labelCell?.text = "\(getItemName(item: m.item)) (x\(m.item.quantity))"
-//        cell.column1.text
         var s: String
         switch m.typeId {
         case 100: s = "retrieve_the_data_96"
@@ -175,10 +242,11 @@ let label = UILabel()
         cell.imageType?.image = image
         // grouped
         if m.group {
-            let image = UIImage(named: "group_96")?.withRenderingMode(.alwaysOriginal)
-            cell.imageGroup?.image = image
+            cell.imageGroup?.image = UIImage(named: "group_96")?.withRenderingMode(.alwaysOriginal)
+        } else {
+            cell.imageGroup?.image = nil
         }
-        // power
+    // power
         if m.powerId > 0,   // powerID начинаются от 1
            m.powerId <= powerVal.count {
             cell.labelPower?.text = "\(powerVal[m.powerId-1])"
@@ -187,13 +255,13 @@ let label = UILabel()
         }
         if m.item.rarId>0,
            m.item.rarId<=4 {
-            cell.backgroundColor = UIColorFromRGB(rgbValue: rareVal[m.item.rarId])
+            cell.backgroundColor = UIColorFromRGB(rgbValue: rareVal[m.item.rarId-1])
         }
       return cell
     }
 
     @objc private func setTitle(newTitle: String) {
-            print("set new title")
+//print("set new title")
             let label = UILabel()
             label.backgroundColor = .clear
             label.numberOfLines = 0     // To remove any maximum limit
@@ -205,15 +273,83 @@ let label = UILabel()
             label.adjustsFontSizeToFitWidth = true
         self.navigationTitle.titleView = label
         }
+    
+    @objc func showJson(data: Data) {
+        filterZones[0] = switchZone0.isOn
+        filterZones[1] = switchZone1.isOn
+        filterZones[2] = switchZone2.isOn
+        filterZones[3] = switchZone3.isOn
+        filterCategories[0] = switchCategory0.isOn
+        filterCategories[1] = switchCategory1.isOn
+        filterCategories[2] = switchCategory2.isOn
+        filterCategories[3] = switchCategory3.isOn
+        zones.removeAll()
+        zones.append([MissionFlat]())
+        zones.append([MissionFlat]())
+        zones.append([MissionFlat]())
+        zones.append([MissionFlat]())
 
-    @IBAction func touchUpInside(_ sender: Any) {
-        for i in 0..<zones.count{
-            zones[i].removeAll()
-        }
         for i in 0..<vbucksSum.count {
             vbucksSum[i] = 0
         }
+        let filterSingle = switchSingle.isOn
+        let filterGroup = switchGroup.isOn
+        let filterVBucks = switchVBucks.isOn
+        var mTmp: MissionFlat! = MissionFlat()
+        do {
+        let decoder = JSONDecoder()
+            let mi = try decoder.decode(PveMissionsInfo.self, from: data)
+        for m in mi.missions {
+            mTmp.categoryId = m.categoryId
+            mTmp.typeId = m.typeId
+            mTmp.group = m.group
+            mTmp.groupMembersCount = m.groupMembersCount
+            mTmp.powerId = m.powerId
+            mTmp.locationName = ""
+            for i in m.items {
+                mTmp.item = i
+                    var filterCategory: Bool = false
+                    if mTmp.categoryId < 0 {
+                        filterCategory = true
+                    } else
+                    if (mTmp.categoryId < self.filterCategories.count) {
+                        filterCategory = self.filterCategories[mTmp.categoryId]
+                    }
+                if filterCategory,
+                   (filterSingle && m.group == false) ||
+                    (filterGroup && m.group == true),
+                  m.location >= 0,
+                  m.location < self.zones.count,
+                  self.filterZones[m.location]{
+                    print(m.location)
+                       print(mTmp!)
+                       mTmp.locationName = self.zonesNames[m.location]
+                        self.zones[m.location].append(mTmp)
+                       if mTmp.item.nameId == 9 {   // V-Bucks
+                            self.vbucksSum[m.location] += mTmp.item.quantity
+                       }
+                }}
+        }
+ //   DispatchQueue.main.async { [self] in
+        expire = mi.expire
+        var i: Int = 0
+        while i<zones.count {
+            if zones[i].isEmpty {zones.remove(at: i)} else {i+=1}   // такое хитрое удаление, т.к. в простом for уедут индексы
+        }
+        tableView.reloadData()
+        label1.text = NSLocalizedString("abb_caps_sw", comment: "") + "\n\(vbucksSum[0])"
+        label2.text = NSLocalizedString("abb_caps_pt", comment: "") + "\n\(vbucksSum[1])"
+        label3.text = NSLocalizedString("abb_caps_cv", comment: "") + "\n\(vbucksSum[2])"
+        label4.text = NSLocalizedString("abb_caps_tp", comment: "") + "\n\(vbucksSum[3])"
+        labelSum.text = NSLocalizedString("element_vbucks_total", comment: "") + "\n\(vbucksSum[0]+vbucksSum[1]+vbucksSum[2]+vbucksSum[3])"
+//    }
+        } catch {
+                print(error.localizedDescription)
+        }
 
+    }
+    
+    @IBAction func touchUpInside(_ sender: Any) {
         let req = NSMutableURLRequest(url: NSURL(string:serverIP)! as URL)
 //        req.httpMethod="GET"
         req.timeoutInterval = 5000.0
@@ -230,51 +366,17 @@ let label = UILabel()
                 print ("server error")
                 return
             }
-            if httpresponse.mimeType != "application/json" {
-               print("content error, mimeType != json")
-               return
-            }
  //print ("got data: \(String(describing: String(data: data!, encoding: .utf8)))")
-            var mTmp: MissionFlat! = MissionFlat()
-            do {
-            let decoder = JSONDecoder()
-            let mi = try decoder.decode(PveMissionsInfo.self, from: data!)
-            for m in mi.missions {
-                mTmp.categoryId = m.categoryId
-  //              mTmp.location = m.location
-                mTmp.typeId = m.typeId
-                mTmp.group = m.group
-                mTmp.groupMembersCount = m.groupMembersCount
-                mTmp.powerId = m.powerId
-                for i in m.items {
-                    mTmp.item = i
-                    print(m.location)
-                    print("zones count=\(self.zones.count)")
-                    print(mTmp!)
-                    
-                    DispatchQueue.main.async{
-                        if m.location >= 0,m.location < self.zones.count {
-                        self.zones[m.location].append(mTmp)
-                            if mTmp.item.nameId == 9 {   // V-Bucks
-                                self.vbucksSum[m.location] += mTmp.item.quantity
-                            }
-                        }
-             }
+            if let mimeType = httpresponse.mimeType, mimeType == "application/json",
+                let data = data {
+                DispatchQueue.main.async {
+                    self.showJson(data: data)
+                }
+            } else {
+                print("content error, mimeType != json")
+                return
             }
-            }
-        DispatchQueue.main.async { [self] in
-            expire = mi.expire
-            tableView.reloadData()
-            label1.text = NSLocalizedString("abb_caps_sw", comment: "") + "\n\(vbucksSum[0])"
-            label2.text = NSLocalizedString("abb_caps_pt", comment: "") + "\n\(vbucksSum[1])"
-            label3.text = NSLocalizedString("abb_caps_cv", comment: "") + "\n\(vbucksSum[2])"
-            label4.text = NSLocalizedString("abb_caps_tp", comment: "") + "\n\(vbucksSum[3])"
-            labelSum.text = NSLocalizedString("element_vbucks_total", comment: "") + "\n\(vbucksSum[0]+vbucksSum[1]+vbucksSum[2]+vbucksSum[3])"
-        }
-            } catch {
-                    print(error.localizedDescription)
-            }
-        }
+       }
         task.resume()
     }
     }
